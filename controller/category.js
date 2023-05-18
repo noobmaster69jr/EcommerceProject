0bf68aa898a91d4e695ab5d47b353ec80c6a964d
noobmaster69jr <srashwin22@gmail.com>
2023-05-18 19:39:06 +0530

    api creation

--- controller/category.js
+++ controller/category.js
@@ -12,7 +12,6 @@ async function createCategory(req, res){
          const result = await Categories.create({name, description})
     console.log('result',result)
     res.send({msg:"Category has been created"})
-
     }catch(err){
         console.log('err in creation of categories')
         res.status(500).send({msg:"Internal sever error"})
@@ -20,6 +19,77 @@ async function createCategory(req, res){
    
 }
 
+
+async function getAllCategory(req, res) {
+  try {
+    const result = await Categories.findAll();
+    console.log("result", result);
+    res.send(result);
+  } catch (err) {
+    console.log("err in retriving  categories");
+    res.status(500).send({ msg: "Internal sever error" });
+  }
+}
+
+async function getCategoryById(req, res) {
+  try {
+    const categoryId = req.params.id
+    const result = await Categories.findOne({
+        where :{
+            id: categoryId
+        }
+    });
+    console.log("result", result);
+    res.send(result);
+  } catch (err) {
+    console.log("err in retriving category by id");
+    res.status(500).send({ msg: "Internal sever error" });
+  }
+}
+
+async function updateCategory(req, res){
+    const categoryId = req.params.id
+      try {
+         const result = await Categories.findOne({
+           where: {
+             id: categoryId,
+           },
+         });
+         if(result){
+            result.name = req.body.name
+            result.description = req.body.description
+            result.save()
+            res.send({msg:'Category got updated', updateCategory: result})
+         }else{
+             console.log("err in retriving  categories");
+             res.status(500).send({ msg: "request id is not present" });
+         }
+        
+      } catch (err) {
+        console.log("err in retriving  categories");
+        res.status(500).send({ msg: "Internal sever error" });
+      }
+}
+
+async function deleteCategory(req, res){
+    const categoryId = req.params.id;
+    try {
+      const result = await Categories.destroy({
+        where: {
+          id: categoryId,
+        },
+      });
+
+      res.send({ msg: "catrgory deleted", result });
+    } catch (err) {
+      console.log(" error in deleting category");
+      res.status(500).send({ msg: "Internal server error" });
+    }
+}
 module.exports = {
-    createCategory
+    createCategory,
+    getAllCategory, 
+    getCategoryById, 
+    updateCategory, 
+    deleteCategory
 }
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const routes = express.Router()
-const {createCategory} = require('../controller/category')
+const {createCategory, getAllCategory, getCategoryById, updateCategory, deleteCategory} = require('../controller/category')
 
 routes.post('/ecomm/api/v1/categories', createCategory)
-routes.get('/ecomm/api/v1/categories')
-routes.put('/ecomm/api/v1/categories/:id')
+routes.get('/ecomm/api/v1/categories', getAllCategory)
+routes.get('/ecomm/api/v1/categories/:id', getCategoryById)
+routes.put('/ecomm/api/v1/categories/:id', updateCategory)
+routes.delete("/ecomm/api/v1/categories/:id", deleteCategory);
 
 module.exports = routes
